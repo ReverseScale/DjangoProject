5c68ead086310a197332e1cd73ae97a876c19df4
ReverseScale <ReverseScale@iCloud.com>
2018-09-18 19:05:38 +0800

    ✨:Introducing new features all urls.

--- 02.Model structure/article/views.py
+++ 02.Model structure/article/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render_to_response, get_object_or_404
 from .models import Article
 
 # Create your views here.
+
+def article_list(request):
+	articles = Article.objects.all()
+	context = {}
+	context['articles'] = articles
+	return render_to_response("article_list.html", context)
+
 def article_detail(request, article_id):
 
 	# get_object_or_404 简化写法
--- 02.Model structure/mysite/urls.py
+++ 02.Model structure/mysite/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from . import views
-from article.views import article_detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('article/<int:article_id>', article_detail, name="article_detail"),
+    path('article/', include('article.urls'))
 ]
